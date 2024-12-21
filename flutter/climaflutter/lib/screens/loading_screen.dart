c5b42b17b9a1a14dd9fe0764d82ebddaeb1f2d09
naaasruul <nasrulhaqhidayat@gmail.com>
2024-12-21 18:03:56 +0800

    setup

---
+++ flutter/climaflutter/lib/screens/loading_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  final LocationSettings locationSettings = LocationSettings(
+    accuracy: LocationAccuracy.low,
+    distanceFilter: 100,
+  );
+  void getLocation() async{
+    Position position = await Geolocator.getCurrentPosition(locationSettings: locationSettings);
+    print(position);
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            //Get the current location
+            getLocation();
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
